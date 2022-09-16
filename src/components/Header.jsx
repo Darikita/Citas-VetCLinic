629220d714bcde8b1ea925b3a8899a1032e003e9
darikita <darikavaldez@gmail.com>
2022-09-15 22:26:24 -0400

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
   return (
     <div className="container p-9 bg-[#ebe3d7] rounded-xl ">
       <header className="md:flex ">
-        <img className=" mx-auto ml-10 w-44" src={logo} />
-        <h1 className="font-black text-[#505050] text-3xl md:text-6xl text-center mr-24 mt-11">
+        <img className=" mx-auto ml-10 md:w-44 w-48" src={logo} />
+        <h1 className="font-black text-[#505050] text-4xl md:text-6xl text-center md:mr-24 mt-11">
           Seguimiento {""}
           <span className="text-[#ff4562]">de Pacientes</span>
         </h1>
